d2c841038f6aed35b0d6fff012b84c21a456c796
omsomso <pkrystian.k@gmail.com>
2023-03-01 12:41:52 +0100

    better sort_med and sort_big

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: kpawlows <kpawlows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 21:18:14 by kpawlows          #+#    #+#             */
-/*   Updated: 2023/02/27 02:24:41 by kpawlows         ###   ########.fr       */
+/*   Updated: 2023/03/01 12:39:47 by kpawlows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,32 +62,107 @@ void	sort_small(t_node **a, t_node **b, int n)
 		do_things(a, b, "pa");
 }
 
-int	numerology(t_node **a, t_node **b, int iter, int n2)
+void	sort_med2(t_node **a, t_node **b, int n)
+{
+	int	count = 0;
+
+	while (count < 2)
+	{
+		while (lst_count_el(a) > 0)//(count < 5)////(lst_check_sort(a) == 0)
+		{
+			count++;
+			if (lst_count_el(b) == 0)
+			{
+				do_things(a, b, "pb");
+			}
+			push_smaller(a, b, (*b)->val);
+			do_things(a, b, "ra");
+				//break ;
+		}
+		while (lst_count_el(b) > 0)
+		{
+			push_larger(a, b, find_median(b));
+			//do_things(a, b, "pa");
+		}
+	}
+}
+
+int	find_split(t_node **stack, int nice)
 {
 	int		val;
 	int		*v;
 	int		n;
-	int		nice;
+	//int		nice;
 
-	n = lst_count_el(a);
+	n = lst_count_el(stack);
 	if (n != 0)
 	{
-		v = lst_to_arr(a, n);
+		v = lst_to_arr(stack, n);
 		ps_sortarr(v, n);
 	}
-	nice = 18 - (iter * 2);
-	if (iter == 1)
-		nice = n / 3;
-	if (iter == 2)
-		nice = n / 4;
-	if (nice >= n || nice > 0)
-		val = find_median(a) / iter + 1;
-	if (n > 0 && nice > 0 && nice < n)
-		val = v[nice];
-	if (nice < 0)
-		val = v[n / 2];
+	//int	r = 4;
+	val = v[n / nice];
+	//if (val < v[0])
+	//	val = -2147483648;
+	//if (n > 0 && nice > 0 && nice < n)
+	//	val = v[nice];
+	//if (nice < 0)
+		//val = v[n / 2];
 	free(v);
 	return (val);
+}
+
+int	find_small_split(t_node **stack, int nice)
+{
+	int		val;
+	int		*v;
+	int		n;
+	static int	iter = 0;
+	//int		nice;
+
+	iter++;
+	n = lst_count_el(stack);
+	if (n != 0)
+	{
+		v = lst_to_arr(stack, n);
+		ps_sortarr(v, n);
+	}
+	if (nice > n)
+		nice = n;
+	val = v[nice];
+	//if (val < v[0])
+	//	val = -2147483648;
+	//if (n > 0 && nice > 0 && nice < n)
+	//	val = v[nice];
+	//if (nice < 0)
+		//val = v[n / 2];
+	free(v);
+	return (val);
+}
+
+void	sort_med3(t_node **a, t_node **b, int n)
+{
+	int	split;
+
+	while (lst_count_el(a) > 0)
+		push_smaller(a, b, find_small_split(a, 10));
+	/*if (lst_count_el(b) != 0)
+	{
+		if (lst_count_el(b) != 0)
+		{
+			if ()
+			while ((*a)-> next)
+			do_things(a, b, "pa");
+		}
+		
+	}*/
+//	while (lst_count_el(a) > 0)
+//		push_smaller(a, b, find_split(a, 3));
+	while (lst_count_el(b) > 0)
+	{
+		rot_to_idx(b, "b", find_largest_idx(b));
+		do_things(a, b, "pa");
+	}
 }
 
 void	sort_med(t_node **a, t_node **b, int n)
@@ -96,12 +171,12 @@ void	sort_med(t_node **a, t_node **b, int n)
 	int	i;
 
 	i = 1;
-	while (lst_count_el(a) > n / 6)
+	while (lst_count_el(a) > n / 10)
 	{
 		push_smaller(a, b, numerology(a, b, i, n));
-		//add_op(a, b);
 		i++;
 	}
+		//add_op(a, b);
 	while (lst_count_el(a) > 3)
 	{
 		rot_to_idx(a, "a", find_smallest_idx(a));
@@ -120,12 +195,17 @@ void	sort_med(t_node **a, t_node **b, int n)
 
 void	sort_big(t_node **a, t_node **b)
 {
-	while (lst_check_sort(a) == 0)
-		push_smaller(a, b, find_median(a));
+	int i = 1;
+	while (lst_count_el(a) > 0)//(lst_check_sort(a) == 0)
+	{
+		push_smaller(a, b, find_split(a, 4));//numerology(a, b, i, 500));
+		//push_smaller(a, b, find_median(a));
+		i++;
+	}
 	while (lst_count_el(b) > 0)
-		push_larger(a, b, find_median(b));
-	while (lst_check_sort(a) == 0)
-		push_smaller(a, b, find_median(a));
+		push_larger(a, b, find_split(b, 8));//find_median(b));
+	while (lst_count_el(a) > 0)
+		push_smaller(a, b, find_split(a, 20));
 	while (lst_count_el(b) > 0)
 	{
 		rot_to_idx(b, "b", find_largest_idx(b));
